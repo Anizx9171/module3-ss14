24f8e54a698f9270e3fb4f33fa08ccf440a277bb
Anizx9171 <anizx9171@gmail.com>
2023-10-18 10:16:58 +0700

    upp

---
+++ src/config/Config.java
@@ -0,0 +1,61 @@
+package config;
+import java.util.Scanner;
+
+public class Config {
+    public static Scanner scanner(){
+        return new Scanner(System.in);
+    }
+    public static int getInt(){
+        Scanner scanner = new Scanner(System.in);
+        int n;
+        while (true){
+            try {
+                System.out.println("Nhập lựa chọn của bạn:");
+                n = Integer.parseInt(scanner.nextLine());
+                return n;
+            }catch (Exception e){
+                System.err.println("lựa chọn không hợp lệ");
+            }
+        }
+    }
+    public static void bubbleSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = array.length - 1; j > i; j--) {
+                if (array[j] < array[j - 1]) {
+                    int temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                }
+            }
+        }
+    }
+    public static void insertionSort(int[] array) {
+        int pos, x;
+        for(int i = 1; i < array.length; i++){ //đoạn array[0] đã sắp xếp
+            x = array[i];
+            pos = i;
+            while(pos > 0 && x < array[pos-1]){
+                array[pos] = array[pos-1]; // đổi chỗ
+                pos--;
+            }
+            array[pos] = x;
+        }
+    }
+
+    public static void selectionSort(int[] array) {
+        int min; // chỉ số phần tử nhỏ nhất trong dãy hiện hành
+        for (int i = 0; i < array.length - 1; i++) {
+            min = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[min]) {
+                    min = j; // ghi nhận vị trí phần tử nhỏ nhất
+                }
+            }
+            if (min != i) {
+                int temp = array[min];
+                array[min] = array[i];
+                array[i] = temp;
+            }
+        }
+    }
+}
